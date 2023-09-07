b28b88baeb3585cd700ad76b86ce80b68f9163b6
rattasat-onnomdee <rattasat0012@gmail.com>
2023-09-07 23:34:28 +0700

    Add new code in Invoice project

--- invoice.c
+++ invoice.c
@@ -9,8 +9,8 @@ void main(){
     printf("Enter your amount and price of Tissue :");
     scanf("%d %d",&aT,&pT);
 
-    printf("Product           Unit    Price   Total\n");
-    printf("\"Coke\"            %d       %d      %d\n",aC,pC,aC*pC);
-    printf("\"Nori Lay\"        %d       %d      %d\n",aN,pN,aN*pN);
-    printf("\"Tissue Paper\"    %d       %d      %d\n",aT,pT,aT*pT);
+    printf("Product\t\tUnit    Price   Total\n");
+    printf("\"Coke\"\t\t%d       %d      %d\n",aC,pC,aC*pC);
+    printf("\"Nori Lay\"\t%d       %d      %d\n",aN,pN,aN*pN);
+    printf("\"Tissue Paper\"\t%d       %d      %d\n",aT,pT,aT*pT);
 }
